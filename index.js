19b6bdf3ced15817bea36c552d28b9ef0e7d2831
MayaMiliani <milianimaya@gmail.com>
2021-06-19 15:08:59 -0300

    Se agrego ventanas modales

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-let logo = document.getElementsByClassName("logo");
-console.log(logo.innerHTML);
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+//Consigna CREA UN ELEMENTO MANIPULANDO EL DOM
+let hero__left = document.getElementsByClassName('hero__left')[0];
+
+let parrafo = document.createElement('p');
+
+parrafo.textContent = "Relaja el cuerpo con jugo fresco"
+
+hero__left.appendChild(parrafo);
+
+
+//Consigna MOFIDICA ETIQUETAS EXISTENTES
+const inicio = document.querySelector('.Inicio')
+
+
+inicio.textContent = "Home"
