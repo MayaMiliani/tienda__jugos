f903c19c6045553a0ff30c8056890525c416ce0e
MayaMiliani <milianimaya@gmail.com>
2021-06-19 17:27:13 -0300

    Botones adaptables a pantalla

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
 
 //Consigna CREA UN ELEMENTO MANIPULANDO EL DOM
 let hero__left = document.getElementsByClassName('hero__left')[0];
@@ -17,3 +14,22 @@ const inicio = document.querySelector('.Inicio')
 
 
 inicio.textContent = "Home"
+
+
+//Variables del proyecto
+const addToCartButtons= document.getElementsByClassName('category__btn');
+let cartContainer = document.getElementsByTagName('tbody')[0];
+let quantityFields = document.getElementsByClassName('num');
+let delete_buttons = document.getElementsByClassName('uk-button-danger');
+
+
+//Seleccionando los botones de agregar al carrito de
+
+for (let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener('click', addToCart)
+
+};
+
+//Funcion para agregar al carrito
+
+
